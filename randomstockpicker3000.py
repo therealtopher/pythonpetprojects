95c9d8e0268b0f68d42ab59e69147335f452200a
Christopher Wheatley <87779008+therealtopher@users.noreply.github.com>
2022-03-22 22:59:22 -0400

    Add files via upload

--- randomstockpicker3000.py
+++ randomstockpicker3000.py
@@ -8,11 +8,23 @@ from yahoo_fin import stock_info as si
 import yfinance as yf
 import random
 
-tickers = si.tickers_sp500()
-luckychoiceticker = tickers[random.randint(0,len(tickers))]
+riskrating = input('On a scale from 1 to 3, how lucky do you feel? ')
+if riskrating == '1':
+    stonks = si.tickers_dow()
+    print("super duper lame")
+    
+elif riskrating == '2':
+    stonks = si.tickers_sp500()
+    print("lame")
+    
+else:
+    print("That's what I like to see")
+    stonks = si.tickers_nasdaq()
+        
+
+luckychoiceticker = stonks[random.randint(0,len(stonks))]
 luckychoiceinfo = yf.Ticker(luckychoiceticker)
 luckychoicedescription = luckychoiceinfo.info
 print(luckychoicedescription['longName'])
 print(luckychoiceticker)
 print(luckychoicedescription['longBusinessSummary'])
-
